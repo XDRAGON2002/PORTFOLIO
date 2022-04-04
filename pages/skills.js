d3c7676cb18c4d654e6c3354aff4dea5a9600cef
DRAGON <anantvijay3@gmail.com>
2022-04-05 01:27:25 +0530

    add icon sphere

--- pages/skills.js
+++ pages/skills.js
@@ -1,6 +1,7 @@
 import SkillCardList from "../components/SkillCardList"
 import { skillItems } from "../data/skills.data"
 import { motion } from "framer-motion"
+import { Cloud } from 'react-icon-cloud'
 
 const pageVariants = {
     initial: {opacity: 0, x: -200, y: -200, scale: 0.8},
@@ -22,9 +23,15 @@ const skills = () => {
                         return <motion.span key = {idx} whileHover = {{scale: 1.5}} className = "flex font-bold text-5xl md:text-9xl">{char}</motion.span>
                     })}
                 </div>
-                <br />
-                <br />
-                <SkillCardList items = {skillItems} row = {1} />
+                <div>
+                    <Cloud id="1" canvasProps={{style : {width: "75vw !important", height: "75vw !important"}}}>
+                        {skillItems.map((skillItem, idx) => {
+                            return <a href="#" key={skillItem.id}>
+                                <img height = "96" width = "96" alt = {skillItem.title} src = {skillItem.link} />
+                            </a>
+                        })}
+                    </Cloud>
+                </div>
                 <br />
                 <br />
             </div>
